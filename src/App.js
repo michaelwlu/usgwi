054fd45daaf36094e1343833ae06e2667cb3881f
brandon.beschta <brandon.beschta@spatialfront.com>
2021-04-15 15:38:19 +0000

    Merged in brandonDev (pull request #1)
    Add ArcGis Map
    
    * Add Map
    
    Approved-by: ashim.ghimire

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+import Map from './containers/Map';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+    <div className='row'>
+      <div className='search-form col-md-4'>
+        <p>Fill in with search form...</p>
+      </div>
+      <Map />
+    </div>
     </div>
   );
 }
---
+++ src/containers/Map.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import MapComponent from '../components/MapComponent';
+
+const Map = () => {
+
+  return (
+		<div className="qsr-map col-md-8">
+      <MapComponent />
+			<div className="webmap" id='viewDiv' />
+		</div>
+	);
+}
+
+export default Map;
