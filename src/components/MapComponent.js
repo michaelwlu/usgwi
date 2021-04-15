e4e8ab5552425e1abee280bdb89b36fc8f2b50b2
Brandon Beschta <brandonbeschta@Brandons-MacBook-Pro.local>
2021-04-15 13:53:12 -0500

    Add city layer

--- src/components/MapComponent.js
+++ src/components/MapComponent.js
@@ -1,26 +1,33 @@
 import { useEffect, useRef } from "react";
-import ArcGISMap from "@arcgis/core/Map";
+import WebMap from "@arcgis/core/WebMap";
+import esriConfig from "@arcgis/core/config";
 import MapView from "@arcgis/core/views/MapView";
 import "../stylesheets/map.css";
 
 const MapComponent = () => {
 	const mapRef = useRef({ view: null });
+	const portalURL = 'https://age.spatialfrontgis.com/portal';
+	const portalId = 'a759d3ccd3754a2e91d307d3a6321dc1';
 
 	useEffect(() => {
 		if (mapRef && mapRef.current) {
-			const map = new ArcGISMap({
-				basemap: "satellite",
+			esriConfig.portalUrl = portalURL;
+			const portalWebMap = new WebMap({
+				portalItem: {
+					id: portalId,
+				},
 			});
 
 			const view = new MapView({
-				map: map,
+				map: portalWebMap,
 				container: "viewDiv",
-				center: [-87.62, 41.87],
-				zoom: 12,
+				center: [-96, 34],
+				zoom: 4,
 			});
 
 			view.when(() => {
 				mapRef.current.view = view;
+				mapRef.current.portalWebMap = portalWebMap;
 			});
 		}
 	}, [mapRef]);
